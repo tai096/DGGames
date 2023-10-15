b0abd9938201687730d6e2ce8f91ea6745c597ff
tai096 <trinhtaii09@gmail.com>
2023-10-15 23:51:22 +0700

    config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html",
+    "./products/templates/**/*.html",
+    "./products/templates/components/**/*.html",
+    "./auth/templates/**/*.html",
+    "./general/templates/**/*.html", ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
