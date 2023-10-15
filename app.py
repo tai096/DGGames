f1ae8c597d416abca7a6144ccb3a5927ff411a7f
tai096 <trinhtaii09@gmail.com>
2023-10-15 22:41:16 +0700

    Fetch data games, genre, platform

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from general.general import general_blueprint
-from auth.auth import auth_blueprint
-from products.products import products_blueprint
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dggames.db'
 db = SQLAlchemy(app)
+
+from general.general import general_blueprint
+from auth.auth import auth_blueprint
+from products.products import products_blueprint
 
 app.register_blueprint(general_blueprint)
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
--- products/products.py
+++ products/products.py
@@ -4,14 +4,25 @@ products_blueprint = Blueprint('products', __name__, template_folder='templates'
 
 @products_blueprint.route('/')
 def index():
-    renderItem = render_template('components/Item.html', var = 'test var')
-    renderSideBar = render_template('components/SideBar.html')
+    from models import Games, Platform, Genre
+
+    # fetch games from db
+    games_query = Games.query.all()
+    platforms_query = Platform.query.all()
+    genres_query = Genre.query.all()
+
+    renderItem = render_template('components/Item.html', games_query = games_query)
+    renderSideBar = render_template('components/SideBar.html', platforms_query = platforms_query, genres_query = genres_query)
     renderDropdown = render_template('components/Dropdown.html')
 
     return render_template('products.html', Item = renderItem, SideBar = renderSideBar, Dropdown = renderDropdown)
 
 @products_blueprint.route('/<product_id>')
 def product_detail(product_id):
-    renderBreadcrumbs = render_template('components/Breadcrumbs.html', var = 'test var')
+    from models import Games
 
-    return render_template('product-detail.html', Breadcrumbs = renderBreadcrumbs)
+    game_query = Games.query.filter_by(id = product_id).first()
+
+    renderBreadcrumbs = render_template('components/Breadcrumbs.html', product_name = game_query.name)
+
+    return render_template('product-detail.html', Breadcrumbs = renderBreadcrumbs, game_query = game_query)
