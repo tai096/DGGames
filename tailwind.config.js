a7f29fd1bb9072f2fe0ce9353dfd9251aac023a4
tai096 <trinhtaii09@gmail.com>
2023-10-16 10:00:39 +0700

    update Readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html",
-    "./products/templates/**/*.html",
-    "./products/templates/components/**/*.html",
-    "./auth/templates/**/*.html",
-    "./general/templates/**/*.html", ],
+  // content: ["./templates/**/*.html",
+  //   "./products/templates/**/*.html",
+  //   "./products/templates/components/**/*.html",
+  //   "./auth/templates/**/*.html",
+  //   "./general/templates/**/*.html", ],
+  content: ["./**/*.html"],
   theme: {
     extend: {},
   },
