e04954bb86b4520516c420d964f808119d71137d
thangdev03 <nhatthangdev@gmail.com>
2023-10-17 01:10:36 +0700

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
   //   "./general/templates/**/*.html", ],
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#8858ed",
+        secondaryColor: "#2b1f41",
+        bgColor: "#0e0e0e",
+        textColor: "#ffff",
+      },
+    },
   },
   plugins: [],
 }
