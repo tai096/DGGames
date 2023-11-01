e4b5c85cfdf2014a14c77e00e1f9032b8a015aa8
JustKiet <lunarthenerd@gmail.com>
2023-11-01 09:19:55 +0700

    auth backend

--- auth/auth.py
+++ auth/auth.py
@@ -1,47 +1,103 @@
-from flask import Blueprint, sqlite3, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, session, flash, get_flashed_messages, url_for
+import sqlite3
+from flask_bcrypt import Bcrypt
 
 auth_blueprint = Blueprint('auth', __name__, template_folder='templates')
 sqldbname = 'instance/dggames.db'
+
 def generateID():
     conn = sqlite3.connect(sqldbname)
     cursor = conn.cursor()
-
     sqlcommand = "SELECT Max(id) from user"
     cursor.execute(sqlcommand)
     max_id = cursor.fetchone()[0]
     return max_id;
 
-def SaveToDB(name, email, password):
+def check_notunique_email(email):
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+    sql_command = (f'Select * from user where email = "{email}"')
+    cursor.execute(sql_command)
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+def check_notunique_username(username):
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+    sql_command = (f'Select * from user where name = "{username}"')
+    cursor.execute(sql_command)
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+def save_to_db(name, username, email, hashed_password, phone_number):
     id_max = generateID()
     if id_max > 0:
-        id_max = id_max + 1
+        id_max += 1
     else: id_max = 1
-    print(id_max)
     conn = sqlite3.connect(sqldbname)
-    cur = conn.cursor()
-    cur.execute("INSERT INTO user (id, name, email, password) VALUES (?, ?, ?, ?)", (id_max, name, email, password))
+    cursor = conn.cursor()
+    sqlcommand = (f'Insert Into user (id, name, username, email, password, phone_number) VALUES ({id_max}, "{name}", "{username}", "{email}", "{hashed_password}", "{phone_number}")')
+    cursor.execute(sqlcommand)
     conn.commit()
     conn.close()
     return id_max
 
-@auth_blueprint.route('/register', methods=['POST'])
+def get_obj_user(username, hashed_password):
+    result = []
+    conn = sqlite3.connect(sqldbname)
+    cursor = conn.cursor()
+    sqlcommand = (f'Select * from user where name = "{username}" and password= "{hashed_password}"')
+    cursor.execute(sqlcommand)
+    obj_user = cursor.fetchone()
+    if len(obj_user) > 0:
+        result = obj_user
+    conn.close()
+    return result
+
+@auth_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
-    username = request.form['username']
-    email = request.form['email']
-    password = request.form['password']
-    username_error = ""; email_error = ""; password_error = ""
-    if not username: username_error = "Username is required."
-    if not password: password_error = "Password is required."
-    if not username_error or password_error:
-        return render_template('register.html',
-                            username_error=username_error,
-                            password_error=password_error,
-                            registration_success="")
-    newid = SaveToDB(username, email, password)
-    stroutput = f'Registered: Username - {username}, Password - {password}'
-    registration_success = "Registration Successful with id = " + str(newid)
-    print(registration_success+stroutput)
-    return render_template('register.html', password_error="", username_error="",registration_success=registration_success+stroutput)
-@auth_blueprint.route('/login')
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        phone = request.form['phone']
+        username_error = ""
+        email_error = ""
+        registration_success = ""
+        if check_notunique_username(username) or check_notunique_email(email):
+            if check_notunique_username(username):
+                username_error = "Username already exists!"
+            if check_notunique_email(email):
+                email_error = "Email already exists!"
+            return render_template('register.html', username_error=username_error, email_error=email_error, 
+                           registration_success="")
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') 
+        new_id = save_to_db(name, username, email, hashed_password, phone)
+        flash(f'Register Successfully, ID = {new_id}', category='success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', username_error="", email_error="", 
+                           registration_success="")
+
+@auth_blueprint.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        obj_user = get_obj_user(username, hashed_password)
+
+        if int(obj_user[0]) > 0:
+            curr_user = {
+                "id": obj_user[0],
+                "name": obj_user[1],
+                "email": obj_user[2]
+            }
+            session['current_user'] = curr_user
+            return redirect(url_for('index'))
+        else:
+            flash("Username and password are not correct!", category='danger')
     return render_template('login.html')
